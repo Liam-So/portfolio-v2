1b0bdfd7302d7cf5cf8d6ffaf188cf1b91cfb0bc
Liam-So <Liam.So@dal.ca>
2025-02-28 15:05:56 -0500

    add solari project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,13 +95,66 @@ export default function Home() {
 
           <div className='py-8'>
             <p className='text-decoration-line: underline'>FinTrack, 2025</p>  
-            <p>Smart Expense Tracker Powered by AI</p>
-            <div className='flex gap-2'>
-            <a className='text-blue-600 hover:text-blue-800 text-sm' href={"https://fintrack-rho.vercel.app/"} target="_blank"
+            <p className='text-gray-700'>An intelligent personal finance platform that automatically categorizes transactions, and provides personalized financial insights.</p>
+            <div className='flex gap-2 pt-2'>
+            <a className='text-sm hover:text-gray-600' href={"https://fintrack-rho.vercel.app/"} target="_blank"
                     rel="noopener noreferrer">View Demo</a>
-              <Link className='text-blue-600 hover:text-blue-800 text-sm' href={"/fintrack"}>View Blog</Link>
-              <a className='text-blue-600 hover:text-blue-800 text-sm' href={"https://github.com/Liam-So/fintrack"} target="_blank"
+              <Link className='text-sm hover:text-gray-600' href={"/fintrack"}>View Blog</Link>
+              <a className='text-sm hover:text-gray-600' href={"https://github.com/Liam-So/fintrack"} target="_blank"
                     rel="noopener noreferrer">View Code</a>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-8 border-b-2">
+          {/* GIF 1 */}
+          <div className="relative aspect-video rounded-lg overflow-hidden">
+          <a href="https://solari-stm.vercel.app/" target="_blank" rel="noopener noreferrer">
+            <Image
+              src="/solari-clock.gif"
+              alt="motion"
+              fill
+              className="object-cover"
+              unoptimized
+            />
+          </a>
+          </div>
+
+          {/* GIF 2 */}
+          <div className="relative aspect-video rounded-lg overflow-hidden">
+          <a href="https://solari-stm.vercel.app/" target="_blank" rel="noopener noreferrer">
+            <Image
+              src="/solari-detail.gif"
+              alt="mta"
+              fill
+              className="object-cover"
+              unoptimized
+            />
+          </a>
+          </div>
+
+          {/* GIF 3 */}
+          <div className="relative aspect-video rounded-lg overflow-hidden">
+            <a href="https://solari-stm.vercel.app/" target="_blank" rel="noopener noreferrer">
+              <Image
+                src="/solari-full.gif"
+                alt="mtr"
+                fill
+                className="object-cover"
+                unoptimized
+              />
+            </a>
+          </div>
+
+          <div className='py-8'>
+            <p className='text-decoration-line: underline'>Digital Solari Board, 2025</p>  
+            <p className='text-gray-700'>Recreation of Grand Central&apos;s iconic Solari board with real-time subway data.</p>
+            <div className='flex gap-2 pt-2'>
+                <a className='text-sm hover:text-gray-600' href={"https://fintrack-rho.vercel.app/"} target="_blank"
+                      rel="noopener noreferrer">View Demo</a>
+                <a className='text-sm hover:text-gray-600' href={"https://github.com/Liam-So/fintrack"} target="_blank"
+                      rel="noopener noreferrer">View Code</a>
             </div>
           </div>
         </div>
@@ -113,14 +166,6 @@ export default function Home() {
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {projects.map((project, index) => (
               <div key={index} className="space-y-3">
-                {/* <div className="relative aspect-video rounded-lg overflow-hidden">
-                  <Image
-                    src={project.image}
-                    alt={project.title}
-                    fill
-                    className="object-cover"
-                  />
-                </div> */}
                 <div className="space-y-1.5">
                   <h3 className="text-decoration-line: underline">{project.title}</h3>
                   <p className="text-gray-600 text-sm">{project.description}</p>
@@ -138,7 +183,7 @@ export default function Home() {
                     href={project.link}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex items-center gap-1 text-blue-600 hover:text-blue-800 text-sm"
+                    className="flex items-center gap-1 text-sm hover:text-gray-600"
                   >
                     View Project <ExternalLink size={14} />
                   </a>
